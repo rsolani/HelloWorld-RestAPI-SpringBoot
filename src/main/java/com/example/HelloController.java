fb64c70018d688f2628b873eb357cace1752eda5
Robertson S. Vicente <Robertson S. Vicente>
2016-12-10 19:36:31 -0300

    ''

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by rober on 10/12/2016.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String Hello()
+    {
+        return "Hello World !";
+    }
+}
